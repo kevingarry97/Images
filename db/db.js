cb0ba2772337913d9ca6b58a10d583112f6f3579
yeex <mozayntwali@gmail.com>
2020-07-19 18:44:25 +0200

    Image uploading working

--- db/db.js
+++ db/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/tiendra', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
+mongoose.connect('mongodb+srv://kevin_garry:Ndahiriwe123@cluster0.ndrrj.mongodb.net/tienda?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
     .then(() => console.log('Connected to mongoDB...!'))
     .catch(err => console.error(`Couldn't connect`))
